22c2743ec0fe36cb9e6010c6a34a08311ccc230a
Reisi <m.reisi@ec.iut.ac.ir>
2022-07-01 09:49:00 +0430

    add some new base classes to the server

---
+++ Server/BaseClasses.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public class Person
+    {
+        public string Name { get; }
+        public string PhoneNumber { get; }
+        public string Address { get; }
+        
+        public Person(string Name, string PhoneNumber, string Address)
+        {
+            this.Name = Name;
+            this.PhoneNumber = PhoneNumber;
+            this.Address = Address;
+        }
+    }
+
+    public class Clerk: Person, ISendAble, IClerk
+    {
+        public long Id { get; set; }
+        public bool IsNew { get; set; }
+        public bool Edited { get; set; }
+        public bool Removed { get; set; }
+        public bool Select { get; set; }
+        public string UserName { get; }
+        public string Password { get; }
+        public bool IsAdmin { get; set; }
+        public Clerk(string Name, string PhoneNumber, string Address, string UserName, string Password, bool IsAdmin)
+            :base(Name, PhoneNumber, Address)
+        {
+            this.UserName = UserName;
+            this.Password = Password;
+            this.IsAdmin = IsAdmin;
+        }
+    }
+
+    public class Customer : Person, ISendAble, ICustomer
+    {
+        public long Id { get; set; }
+        public bool IsNew { get; set; }
+        public bool Edited { get; set; }
+        public bool Removed { get; set; }
+        public bool Select { get; set; }
+
+        public long Balance { get; set; }
+        public long OrderCountRecive { get; set; }
+        public long OrderCountRemove { get; set; }
+        public string SubscribeCode { get; }
+
+        public Customer(string Name, string PhoneNumber, string Address, string SubscribeCode)
+            : base(Name, PhoneNumber, Address)
+        {
+            this.SubscribeCode = SubscribeCode;
+            OrderCountRecive = 0;
+            OrderCountRemove = 0;
+            Balance = 0;
+        }
+    }
+
+    class Cake: IProduct, ISendAble
+    {
+        public long Id { get; set; }
+        public bool IsNew { get; set; }
+        public bool Edited { get; set; }
+        public bool Removed { get; set; }
+        public bool Select { get; set; }
+
+        public long Price { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Code { get; set; }
+
+        public Cake(long Price, string Name, string Description, string Code)
+        {
+            this.Price = Price;
+            this.Name = Name;
+            this.Description = Description;
+            this.Code = Code;
+        }
+    }
+}
---
+++ Server/BaseInterfaces.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    interface Itransfer
+    {
+        string Read();
+        string Write(string json);
+    }
+    interface ISendAble
+    {
+        long Id { get;}
+        bool IsNew { get; }
+        bool Edited { get;}
+        bool Removed { get;}
+        bool Select { get; }
+    }
+    interface IProduct
+    {
+        long Price { get;}
+        string Name { get;}
+        string Description { get;}
+        string Code { get; }
+    }
+    
+    interface ICustomer
+    {
+        long Balance { get; set; }
+        long OrderCountRecive { get; set; }
+        long OrderCountRemove { get; set; }
+        string SubscribeCode { get; }
+    }
+
+    interface IClerk
+    {
+        string UserName { get; }
+        string Password { get; }
+        bool IsAdmin { get; set; }
+    }
+}
